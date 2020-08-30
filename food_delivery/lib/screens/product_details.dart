c003b9cf723488285c5f4a8fc58c84273f6663e3
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-08-30 02:37:38 +0200

    Product Detail - Add to bag

--- food_delivery/lib/screens/product_details.dart
+++ food_delivery/lib/screens/product_details.dart
@@ -20,88 +20,144 @@ class _ProductDetailsState extends State<ProductDetails> {
     return Scaffold(
       backgroundColor: white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              child: Stack(
-                children: [
-                  Carousel(
-                    images: [
-                      AssetImage('images/${widget.product.image}'),
-                      AssetImage('images/${widget.product.image}'),
-                      AssetImage('images/${widget.product.image}')
-                    ],
-                    dotBgColor: white,
-                    dotColor: grey,
-                    dotIncreasedColor: red,
-                    dotIncreaseSize: 1.2,
-                    autoplay: false,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        icon: Icon(
-                          Icons.arrow_back_ios,
-                          color: black,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                child: Stack(
+                  children: [
+                    Carousel(
+                      images: [
+                        AssetImage('images/${widget.product.image}'),
+                        AssetImage('images/${widget.product.image}'),
+                        AssetImage('images/${widget.product.image}')
+                      ],
+                      dotBgColor: white,
+                      dotColor: grey,
+                      dotIncreasedColor: red,
+                      dotIncreaseSize: 1.2,
+                      autoplay: false,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.arrow_back_ios,
+                            color: black,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
                         ),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Stack(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Image.asset(
-                                "images/shopping-bag.png",
-                                width: 25,
-                                height: 25,
-                              ),
-                            ),
-                            Positioned(
-                              right: 5,
-                              bottom: 0,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: white,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: grey,
-                                        offset: Offset(2, 3),
-                                        blurRadius: 3),
-                                  ],
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Stack(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.asset(
+                                  "images/shopping-bag.png",
+                                  width: 25,
+                                  height: 25,
                                 ),
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.only(left: 4, right: 4),
-                                  child: CustomText(
-                                    text: "2",
-                                    color: red,
-                                    size: 14,
-                                    weight: FontWeight.bold,
+                              ),
+                              Positioned(
+                                right: 5,
+                                bottom: 0,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: white,
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: grey,
+                                          offset: Offset(2, 3),
+                                          blurRadius: 3),
+                                    ],
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 4, right: 4),
+                                    child: CustomText(
+                                      text: "2",
+                                      color: red,
+                                      size: 14,
+                                      weight: FontWeight.bold,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Positioned(
+                      right: 14,
+                      bottom: 14,
+                      child: LikeWidget(),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CustomText(
+                    text: widget.product.name,
+                    size: 24,
+                    weight: FontWeight.bold,
                   ),
-                  Positioned(
-                    right: 14,
-                    bottom: 14,
-                    child: LikeWidget(),
-                  )
+                  CustomText(
+                    text: "\$" + widget.product.price.toString(),
+                    size: 20,
+                    weight: FontWeight.w600,
+                  ),
                 ],
               ),
-            )
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.remove,
+                        size: 28,
+                      ),
+                      onPressed: () {}),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: red,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(28, 16, 28, 16),
+                          child: CustomText(
+                            text: "Add To Bag",
+                            color: white,
+                            size: 24,
+                            weight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                      icon: Icon(
+                        Icons.add,
+                        color: red,
+                        size: 28,
+                      ),
+                      onPressed: () {}),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
