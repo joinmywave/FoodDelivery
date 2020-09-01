ad1378d82d5ed8a58e03cbb546136a370e354f21
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-09-01 09:38:56 +0200

    User authentication and Registration working

--- food_delivery/lib/main.dart
+++ food_delivery/lib/main.dart
@@ -1,22 +1,43 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/providers/auth.dart';
 import 'package:food_delivery/screens/home.dart';
+import 'package:food_delivery/screens/login_screen.dart';
+import 'package:food_delivery/widgets/loading.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: AuthProvider.initialize())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Food Delivery App',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ScreenController(),
+      )));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ScreenController extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Home(),
-    );
+    final auth = Provider.of<AuthProvider>(context);
+    switch (auth.status) {
+      case Status.Uninitialized:
+        return Loading();
+      case Status.Unauthenticated:
+      case Status.Authenticating:
+        return LoginScreen();
+      case Status.Authenticated:
+        return Home();
+      default:
+        return LoginScreen();
+    }
   }
 }
--- food_delivery/lib/screens/registration_screen.dart
+++ food_delivery/lib/screens/registration_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/helpers/screen_navigation.dart';
 import 'package:food_delivery/helpers/style.dart';
+import 'package:food_delivery/providers/auth.dart';
 import 'package:food_delivery/widgets/custom_text.dart';
+import 'package:food_delivery/widgets/loading.dart';
+import 'package:provider/provider.dart';
 
 import 'home.dart';
 import 'login_screen.dart';
@@ -16,120 +19,134 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
     return Scaffold(
       key: _key,
       backgroundColor: white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  "images/lg.png",
-                  width: 240,
-                  height: 240,
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: grey),
-                    borderRadius: BorderRadius.circular(15)),
-                child: Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Name",
-                        icon: Icon(Icons.person)),
+      body: authProvider.status == Status.Authenticating
+          ? Loading()
+          : SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: grey),
-                    borderRadius: BorderRadius.circular(15)),
-                child: Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Email",
-                        icon: Icon(Icons.email)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        "images/lg.png",
+                        width: 240,
+                        height: 240,
+                      ),
+                    ],
                   ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: grey),
-                    borderRadius: BorderRadius.circular(15)),
-                child: Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Password",
-                        icon: Icon(Icons.lock)),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: grey),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          controller: authProvider.name,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Name",
+                              icon: Icon(Icons.person)),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: GestureDetector(
-                onTap: () async {
-                  navigateTo(context, Home());
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: red,
-                      border: Border.all(color: grey),
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: grey),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          controller: authProvider.email,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Email",
+                              icon: Icon(Icons.email)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: grey),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          obscureText: true,
+                          controller: authProvider.password,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Password",
+                              icon: Icon(Icons.lock)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: GestureDetector(
+                      onTap: () async {
+                        if (!await authProvider.signUp()) {
+                          _key.currentState.showSnackBar(SnackBar(
+                            content: Text("Signup failed!"),
+                          ));
+                          return;
+                        }
+                        authProvider.clearController();
+                        navigateToWithReplacement(context, Home());
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: red,
+                            border: Border.all(color: grey),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 10, bottom: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              CustomText(
+                                text: "Register",
+                                color: white,
+                                size: 22,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      navigateTo(context, LoginScreen());
+                    },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         CustomText(
-                          text: "Register",
-                          color: white,
-                          size: 22,
-                        )
+                          text: "Login",
+                          size: 20,
+                        ),
                       ],
                     ),
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                navigateTo(context, LoginScreen());
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CustomText(
-                    text: "Login",
-                    size: 20,
                   ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
